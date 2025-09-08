90a99ad6c701b316de125a2d1c3e2d6a3a79a9c4
Nic Doelger <doelgern@gmail.com>
2025-09-08 12:56:19 -0400

    supabase connected + can register mood

--- App.tsx
+++ App.tsx
@@ -1,20 +1,76 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Pressable } from 'react-native';
+import 'react-native-url-polyfill/auto';
+import { supabase } from './util/supabase';
 
 export default function App() {
+  const [curMood, setMood] = useState({ mood: '' });
+
+  const handleSend = async () => {
+    console.log('hey');
+    try {
+      const { data, error } = await supabase
+        .from('e_or_n_test')
+        .insert(curMood); // or .insert([{ mood: curMood.mood }])
+
+      if (error) throw error;
+      console.log('Mood registered:', data);
+    } catch (err: any) {
+      console.log(err?.message ?? err);
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View style={styles.appContainer}>
+      <Pressable
+        style={{
+          ...styles.pressable,
+          backgroundColor: curMood.mood === 'Exhausted' ? 'green' : 'white',
+        }}
+        onPress={() => {
+          setMood({ mood: 'Exhausted' });
+        }}
+      >
+        <Text style={styles.text}>Exhausted</Text>
+      </Pressable>
+      <Pressable
+        style={{
+          ...styles.pressable,
+          backgroundColor: curMood.mood === 'Nauseous' ? 'green' : 'white',
+        }}
+        onPress={() => {
+          setMood({ mood: 'Nauseous' });
+        }}
+      >
+        <Text style={styles.text}>Nauseous</Text>
+      </Pressable>
+      <Pressable style={styles.pressable} onPress={handleSend}>
+        <Text style={styles.text}>Send</Text>
+      </Pressable>
+      <Text>{curMood.mood}</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+  appContainer: {
+    margin: 'auto',
+    padding: 50,
+    paddingHorizontal: 16,
+    marginTop: 20,
+    height: '100%',
+    gap: 20,
+    flexDirection: 'column',
     justifyContent: 'center',
+  },
+  text: {
+    fontSize: 50,
+    textAlign: 'center',
+    padding: 10,
+  },
+  pressable: {
+    borderWidth: 2,
   },
 });
---
+++ app.config.js
@@ -0,0 +1,35 @@
+import 'dotenv/config';
+
+export default {
+  expo: {
+    name: 'EON',
+    slug: 'EON',
+    version: '1.0.0',
+    extra: {
+      EXPO_PUBLIC_SUPABASE_URL: process.env.EXPO_PUBLIC_SUPABASE_URL,
+      EXPO_PUBLIC_SUPABASE_ANON_KEY: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY,
+    },
+    icon: './assets/icon.png',
+    userInterfaceStyle: 'light',
+    newArchEnabled: true,
+    splash: {
+      image: './assets/splash-icon.png',
+      resizeMode: 'contain',
+      backgroundColor: '#ffffff',
+    },
+    ios: {
+      supportsTablet: true,
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: './assets/adaptive-icon.png',
+        backgroundColor: '#ffffff',
+      },
+
+      edgeToEdgeEnabled: true,
+    },
+    web: {
+      favicon: './assets/favicon.png',
+    },
+  },
+};
